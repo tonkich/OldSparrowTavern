1dd3ea78cdae15235c1f3264e7692d06cdbec9ec
tonko <tonko@DESKTOP-CF20E20>
2022-04-22 10:28:51 +0300

    Add project files.

---
+++ OldSparrowTavern/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OldSparrowTavern.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext()
+            : base("OldSparrowTavernDb", throwIfV1Schema: false)
+        {
+        }
+        public virtual IDbSet<Item> Items { get; set; }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Item>().ToTable("Items");
+        }
+    }
+}
